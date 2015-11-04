7d4640280f8a49d4dc0460888e88e2ba02a3729c
Axel Sundbom <axel@rwnd-main.rewind.aax.se>
2015-11-04 13:47:32 +0000

    Adds connector for MySQL.

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+#sqlite3
 DATABASE = 'database.db'
 
-from flask import Flask, render_template, g #Web server
-import sqlite3 #DB support
+#mysql
+HOST=''
+PORT=''
+USER=''
+PASSWD=''
+SQLDB=''
+
+from flask import Flask, render_template, request, g #Web server
+import sqlite3, MySQLdb #DB support
 
 app = Flask(__name__)
 
@@ -9,9 +17,14 @@ app = Flask(__name__)
 def connect_to_database_sqlite3():
 	return sqlite3.connect(DATABASE)
 
+#returns a database connection for MySQL
+def connect_to_database_mysql():
+	return MySQLdb.connect(host=HOST, port=PORT, user=USER, passwd=PASSWD, db=SQLDB)
+
 #set this line to define database connection
 DBFUNC = connect_to_database_sqlite3
 
+##Setup DB connection
 @app.before_request
 def before_request():
 	try:
@@ -19,6 +32,7 @@ def before_request():
 	except Exception as e:
 		print e
 
+##Kill DB connection
 @app.teardown_request
 def teardown_request(exception):
 	db = getattr(g, 'db', None)
@@ -30,13 +44,21 @@ def teardown_request(exception):
 #main page
 @app.route("/")
 def main():
-	t = ("tbl1",)
+	t = ("*",)
 	try:
-		cur = g.db.execute('SELECT ? FROM tbl1',t)
+		cur = g.db.execute('SELECT * FROM tbl1')
 		print cur.fetchall()
 	except Exception as e:
 		print e
 	return render_template("index.html")
 
+@app.route("/home")
+def home():
+	try:
+		print request.args.get('username') 
+	except Exception as e:
+		print e
+	return render_template("index.html")
+
 if __name__ == "__main__":
-	app.run(host='192.168.1.6')
+	app.run(host='192.168.1.6', port=5000)
