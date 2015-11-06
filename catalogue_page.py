c89bc7ea9120e7995ac6b69c142333f30ce0663a
ax-rwnd <axel-sundbom@hotmail.com>
2015-11-06 01:55:50 +0000

    General face-lift patches.

--- catalogue_page.py
+++ catalogue_page.py
@@ -5,10 +5,9 @@ catalogue_page = Blueprint('catalogue_page', __name__, template_folder='template
 #render catalogue with listed categories
 @catalogue_page.route("/catalogue/")
 def show_catalogue_index():
-	return render_template("catalogue.html", catname='Index')
+	return render_template("catalogue.html", catname='Categories')
 
-#render catalogue with products
+#render catalogue with product
 @catalogue_page.route("/catalogue/<catname>")
 def show_catalogue(catname):
-	#fetch items from catalogue to insert into template
 	return render_template("catalogue.html", catname=catname)
