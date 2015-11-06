ca11966c1e5d2e6693e499ae09e68bcaac61365c
ax-rwnd <axel-sundbom@hotmail.com>
2015-11-06 01:12:33 +0000

    Initial edits for catalogue rendering.

--- catalogue_page.py
+++ catalogue_page.py
@@ -2,7 +2,13 @@ from flask import Blueprint, render_template
 
 catalogue_page = Blueprint('catalogue_page', __name__, template_folder='templates')
 
-@catalogue_page.route("/catalogue")
-def show_catalogue():
+#render catalogue with listed categories
+@catalogue_page.route("/catalogue/")
+def show_catalogue_index():
+	return render_template("catalogue.html", catname='Index')
+
+#render catalogue with products
+@catalogue_page.route("/catalogue/<catname>")
+def show_catalogue(catname):
 	#fetch items from catalogue to insert into template
-	return render_template("catalogue.html")
+	return render_template("catalogue.html", catname=catname)
