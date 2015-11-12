66cf1acace05db0ab41864208a04bace3793a755
kurt@buck.ludd.ltu.se <kurt@buck.ludd.ltu.se>
2015-11-12 20:45:28 +0100

    Products are now browsable in the catalogue page, they also have their own infopage.

--- catalogue_page.py
+++ catalogue_page.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, g
+from itertools import product
 
 catalogue_page = Blueprint('catalogue_page', __name__, template_folder='templates')
 
@@ -15,18 +16,34 @@ def show_catalogue(catname):
 	cats = read_categories()
 	return render_template("catalogue.html", catname=catname, c = cats, p = products)
 
+@catalogue_page.route("/catalogue/<catname>/<prodname>")
+def show_product(catname, prodname):
+	product_info = read_product_info(prodname)
+	cats = read_categories()
+	return render_template("catalogue.html", catname=catname, c = cats, prod = product_info)
+
+def read_product_info(prodname):
+	db = getattr(g, 'db', None)
+	cursor = db.cursor()
+	query = ("select name, description, price from tbl_product where name = %s;")
+	data = (prodname,)
+	cursor.execute(query, data)
+	
+	prod_info = cursor.fetchone()
+	
+	return prod_info
+
 #Read products from catalogue where catname is correct
 def read_products(catname):
 	db = getattr(g, 'db', None)
 	cursor = db.cursor()
-	query = ("select product.name, description, price from tbl_product join\
-		category on tbl_product.cat_id = tbl_category.id where tbl_category.name = %s;")
+	query = ("select tbl_product.name, description, price from tbl_product join\
+		tbl_category on tbl_product.cat_id = tbl_category.id where tbl_category.name = %s;")
 	data = (catname,)
 	cursor.execute(query, data)
 	prod = []
 		
 	for x in cursor:
-		print x
 		prod.append(x)
 	
 	return prod
