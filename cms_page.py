0048576f47e8f69bc6d58ea728457a6a8f1693b8
Kurt <kurt@buck.ludd.ltu.se>
2015-11-19 16:28:36 +0100

    Content Management System working, categories and products can be added and removed. Image file upload is also suported.

---
+++ cms_page.py
@@ -0,0 +1,149 @@
+import os
+from flask import Blueprint, request, render_template, g
+from config import config
+from werkzeug import secure_filename
+
+config['UPLOAD_FOLDER'] = 'static/img'
+config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
+
+cms_page = Blueprint('cms_page', __name__, template_folder='templates')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in config['ALLOWED_EXTENSIONS']
+
+# render catalogue with listed categories
+@cms_page.route("/cms/")
+def show_cms():
+	return render_template("cms.html", editname="")
+
+# render catalogue with product
+@cms_page.route("/cms/<editname>")
+def show_cms_editor(editname):
+	if editname == "Remove Category":
+		cats = read_categories()
+		return render_template("cms.html", editname=editname, cats = cats)
+	elif editname == "Add Product":
+		cats = read_categories()
+		return render_template("cms.html", editname=editname, cats = cats)
+	elif editname == "Remove Product":
+		prod = read_products()
+		cats = read_categories()
+		return render_template("cms.html", editname=editname, cats = cats, prod = prod)
+
+	return render_template("cms.html", editname=editname)
+
+@cms_page.route("/cms/Add Product", methods=['POST'])
+def add_product():
+	prodname = request.form['prodname']
+	prodprice = request.form['prodprice']
+	proddesc = request.form['proddesc']
+	prodcat = request.form['prodcat']
+	prodfile = request.files['prodfile']
+	produrl = ""
+	if prodfile and allowed_file(prodfile.filename) and not os.path.isfile(config['UPLOAD_FOLDER'] + "/" + prodfile.filename):
+		# Make the filename safe, remove unsupported chars
+		filename = secure_filename(prodfile.filename)
+		prodfile.save(os.path.join(config['UPLOAD_FOLDER'], filename))
+		produrl = config['UPLOAD_FOLDER'] + "/" + filename
+	else:
+		print "Image Upload Error"
+	db = getattr(g, 'db', None)
+	try:
+		query = "INSERT INTO tbl_product (name, description, image_url, price, cat_id) VALUES (%s, %s, %s, %s, (SELECT id from tbl_category WHERE name=%s));"
+		with db as cursor:
+			data = (prodname, proddesc, produrl, prodprice, prodcat)
+			cursor.execute(query, data)
+			db.commit()
+	except Exception as e:
+		print e
+	cats = read_categories()
+	return render_template("cms.html", editname = "Add Product", cats = cats)
+
+
+@cms_page.route("/cms/Remove Product", methods=['POST'])
+def remove_product():
+    prodname = request.form['removeprod']
+    db = getattr(g, 'db', None)
+    url = ""
+    try:
+        query = "SELECT image_url from tbl_product WHERE name = %s"
+        with db as cursor:
+            data = (prodname)
+            cursor.execute(query, data)
+            url = cursor.fetchone()[0]
+    except Exception as e:
+        print e
+    os.remove(url)
+    db = getattr(g, 'db', None)
+    try:
+        query = "DELETE FROM tbl_product WHERE name = %s"
+        with db as cursor:
+            data = (prodname)
+            cursor.execute(query, data)
+            db.commit()
+    except Exception as e:
+        print e
+    p = read_products()
+    return render_template("cms.html", editname = "Remove Product", prod = p)
+
+
+@cms_page.route("/cms/Add Category", methods=['POST'])
+def add_category():
+	catname = request.form['catname']
+	cats = read_categories()
+	if catname in cats:
+		print "Category Already Exists"
+	else:
+		db = getattr(g, 'db', None)
+		try:
+			query = "insert into tbl_category (name) VALUES (%s);";
+			with db as cursor:
+				cursor.execute(query, catname)
+				db.commit()
+		except Exception as e:
+			print e
+
+	return render_template("cms.html", editname = "Add Category")
+
+
+@cms_page.route("/cms/Remove Category", methods=['POST'])
+def remove_category():
+	catname = request.form['removecat']
+	db = getattr(g, 'db', None)
+	try:
+		query = "DELETE FROM tbl_category WHERE name = %s;";
+		with db as cursor:
+			cursor.execute(query, catname)
+			db.commit()
+	except Exception as e:
+		print e
+
+	cats = read_categories()
+	return render_template("cms.html", editname = "Remove Category", cats=cats)
+
+# Read categories fom database and returm them as a list
+def read_categories():
+	db = getattr(g, 'db', None)
+	cursor = db.cursor()
+	cursor.execute("select name from tbl_category;")
+
+	cats = []
+
+	for x in cursor:
+		cats.append(x[0])
+
+	return cats
+
+
+#Read products from catalogue where catname is correct
+def read_products():
+	db = getattr(g, 'db', None)
+	cursor = db.cursor()
+	query = "select name from tbl_product;"
+	cursor.execute(query)
+	prod = []
+
+	for x in cursor.fetchall():
+		prod.append(x[0])
+
+	return prod
