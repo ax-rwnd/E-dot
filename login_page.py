00a81d82df2f5bf1d63dbe03c2bbbb16c89a5417
Kurt <kurt@buck.ludd.ltu.se>
2015-11-21 03:32:22 +0100

    Implemented a better way of handling the accounts as a purpose, it now works on email, by that the users wont have to keep track of an identical usename.

--- login_page.py
+++ login_page.py
@@ -6,45 +6,70 @@ from user import User
 login_page = Blueprint('login_page', __name__, template_folder='templates')
 logout_page = Blueprint('logout_page', __name__, template_folder='templates')
 
+
 @login_page.route("/login")
 def show_login():
-	#determine whether the user is logged in or not
-	if current_user.is_authenticated:
-		return "You're already authenticated, user "\
-			+str(current_user.get_id()) + "."
+    # determine whether the user is logged in or not
+    if current_user.is_authenticated:
+        return "You're already authenticated, user " \
+               + str(current_user.get_id()) + "."
 
-	return render_template("login.html")
+    return render_template("login.html")
+
 
 @login_page.route("/logout")
 def show_logout():
-	logout_user()
-	return 'You have been logged out. \
-	<br/>Follow <a href="/">this</a> link to proceed.'
+    logout_user()
+    return render_template("index.html", message="You have been logged out.", status = True)
 
-#main login routine
+
+# main login routine
 @login_page.route("/login", methods=['POST'])
 def show_login_post():
-	db = getattr(g, 'db', None)
-	try :
-		#prepare query
-		name = (request.form['usertext'],)
-		password = (request.form['pwtext'],)
-		query = "select (password) from tbl_user where username = (%s);"
+    db = getattr(g, 'db', None)
+    try:
+        email = (request.form['emailtext'])
+        password = request.form['pwtext']
+        query = "select id, email, password from tbl_user WHERE email = (%s);"
+        pw = ""
 
-		with db as cursor:
-			cursor.execute(query,name)
-			pw = cursor.fetchone()[0]
+        with db as cursor:
+            cursor.execute(query, email)
+            row = cursor.fetchall()
+            uid = row[0][0]
+            em = row[0][1]
+            pw = row[0][2]
 
-		if check_password_hash(pw, password[0]):
-			with db as cursor:
-				query = "select (id) from tbl_user where username = (%s);"
-				cursor.execute(query,name)
-				uid = cursor.fetchall()[0][0]
+        if check_password_hash(pw, password) and (email == em):
+            login_user(User(uid))
+            print "yay"
+            return render_template("basket.html", message="You have been logged in!", status = False)
 
-			login_user(User(uid))
-			return 'You have been logged in! \
-			<br/>Follow <a href="/">this</a> link to proceed.'
-	except Exception as e:
-		print e
-	return 'Login failed.<br/>Follow <a href="/login">this</a> \
-		 link to try again.'
+    except Exception as e:
+        print e
+
+    return render_template("login.html", message="Login failed.")
+
+    # try:
+    #     # prepare query
+    #     name = (request.form['usertext'],)
+    #     password = (request.form['pwtext'],)
+    #     query = "select (password) from tbl_user where username = (%s);"
+    #
+    #     with db as cursor:
+    #         cursor.execute(query, name)
+    #         pw = cursor.fetchone()[0]
+    #
+    #     if check_password_hash(pw, password[0]):
+    #         with db as cursor:
+    #             query = "select (id) from tbl_user where username = (%s);"
+    #             cursor.execute(query, name)
+    #             uid = cursor.fetchall()[0][0]
+    #
+    #         login_user(User(uid))
+    #         return 'You have been logged in! \
+		# 	<br/>Follow <a href="/">this</a> link to proceed.'
+    # except Exception as e:
+    #     print e
+    # return 'Login failed.<br/>Follow <a href="/login">this</a> \
+		#  link to try again.'
--- signup_page.py
+++ signup_page.py
@@ -5,58 +5,66 @@ signup_page = Blueprint('signup_page', __name__, template_folder='templates')
 
 @signup_page.route("/signup")
 def show_signup():
-	return render_template("signup.html")
+    return render_template("signup.html")
 
+# policy check
+def password_policy(pwd):
+    if not pwd:
+        return False
+    elif (len(pwd) < 8 or len(pwd) > 32):
+        return False
+    else:
+        # TODO: add upper+lower+num check, use regex?
+        return True
+
+def email_validation(txt):
+    if (-1 == txt.find('@')):
+        return False
+    elif (-1 == txt.find('.')):
+        return False
+    else:
+        return True
 
 @signup_page.route("/signup", methods=['POST'])
 def show_signup_post():
-	db = getattr(g, 'db', None)
-	#fmt = getattr(g, 'fmt', None)
 
-	#policy check
-	def password_policy(pwd):
-		if not pwd:
-			return False
-		elif (len(pwd)<8 or len(pwd)>32):
-			return False
-		else:
-			#TODO: add upper+lower+num check, use regex?
-			return True
-	
-	def email_validation(txt):
-		if (-1 == txt.find('@')):
-			return False
-		elif (-1 == txt.find('.')):
-			return False
-		else:
-			return True
+    email = request.form['emailtext']
+    passwd = request.form['pwtext']
+    repass = request.form['pwrepeat']
+    nametext = request.form['nametext']
+    addresstext = request.form['addresstext']
+    postcodetext = request.form['postcodetext']
+    citytext = request.form['citytext']
+    country = request.form['countrytext']
 
-	user = request.form['usertext']
-	email = request.form['emailtext']
-	passwd = request.form['pwtext']
-	repass = request.form['repwtext']
+    if not(email and passwd and nametext and addresstext and postcodetext and citytext and country):
+        return render_template("signup.html", message = "Registration Complete")
+    elif email and not email_validation(email):
+        return render_template("signup.html", message = "Invalid Email")
+    elif not password_policy(passwd) or not (passwd == repass):
+        return render_template("signup.html", message = "Invalid Password")
 
-	#validate form
-	if not user or len(user)>16:
-		return 'Invalid username.<br/>Follow <a href="/signup">this</a> \
-			link to try again.'
-	elif email and not email_validation(email):
-		return 'Invalid e-mail, make sure you\'ve typed it in correctly.<br/> \
-			Follow <a href="/signup">this</a> link to try again.'
-	elif not password_policy(passwd):
-		return 'Invalid password.<br/>Follow <a href="/signup">this</a> \
-			link to try again.'
-	elif not (passwd == repass):
-		return 'The passwords you entered did not match.<br/>Follow \
-			<a href="/signup">this</a> link to try again.'
-	else:
-		cursor = g.db.cursor()
-		if email:
-			data = (user, generate_password_hash(passwd), email)
-		else:
-			data = (user, generate_password_hash(passwd))
+    db = getattr(g, 'db', None)
 
-		cursor.execute ("INSERT INTO tbl_user (username, password"+("" if not email else ", email")+ ") values (%s, %s" + ("" if not email else ", %s")+");", data)
-		db.commit()
-		return 'Registration okay!\
-			<br/>Follow <a href="/">this</a> link to proceed.'
+    try:
+        query = ""
+        with db as cursor:
+            data = (email)
+            query = "SELECT * FROM tbl_user WHERE email=%s;"
+            cursor.execute(query, data)
+            if cursor.fetchone() > 0:
+                return render_template("signup.html", message = "Email Address Already Registered.")
+    except Exception as e:
+        print e
+
+    try:
+        query = ""
+        with db as cursor:
+            data = (email, generate_password_hash(passwd), nametext, addresstext, postcodetext, citytext, country)
+            query = "INSERT INTO tbl_user (email, password, name, address, postcode, city, country) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+            cursor.execute(query, data)
+            db.commit()
+    except Exception as e:
+        print e
+
+    return render_template("login.html", message = "Registration Complete", status = true)
